e67e008686428641a5c8271d43ae0487e488cc4d
Anna Makarudze <amakarudze@gmail.com>
2020-03-04 20:26:21 +0200

    Fix validate_postcode function

--- postcodes_api/views.py
+++ postcodes_api/views.py
@@ -6,6 +6,10 @@ from django.shortcuts import HttpResponse
 def validate_postcode(request, code):
     # Format postcode into uppercase
     postcode = code.upper()
+    print(postcode)
+    if len(postcode) < 5 or len(postcode) > 8:
+        raise ValueError(f"Invalid postcode - {postcode}.")
+
     pattern = r"^(([A-Z]{1,2}[0-9][A-Z0-9]?|ASCN|STHL|TDCU|BBND|[BFS]IQQ|PCRN|TKCA) " \
               r"?[0-9][A-Z]{2}|BFPO ?[0-9]{1,4}|(KY[0-9]|MSR|VG|AI)[ -]?[0-9]{4}|[A-Z]{2} " \
               r"?[0-9]{2}|GE ?CX|GIR ?0A{2}|SAN ?TA1)$"
@@ -14,10 +18,22 @@ def validate_postcode(request, code):
         # Check for whitespace between outer and inner parts and add one if absent
         if postcode[-4] == " ":
             formatted_postcode = postcode
+            print(formatted_postcode)
         else:
-            formatted_postcode = postcode[:-4] + " " + postcode[-3:]
-        return formatted_postcode
+            if len(postcode) == 5:
+                formatted_postcode = postcode[:2] + " " + postcode[2:]
+                print(formatted_postcode)
+            elif len(postcode) == 6:
+                formatted_postcode = postcode[:3] + " " + postcode[3:]
+                print(formatted_postcode)
+            elif len(postcode) == 7:
+                formatted_postcode = postcode[:4] + " " + postcode[4:]
+                print(formatted_postcode)
+            else:
+                raise ValueError("Invalid postcode.")
+
+        message = f"{formatted_postcode} is a valid UK postcode."
+
+        return HttpResponse(message)
     else:
-        raise ValueError("Invalid postcode.")
-    formatted_postcode = None
-    return HttpResponse(formatted_postcode)
+        raise ValueError(f"Invalid postcode - {postcode}.")
