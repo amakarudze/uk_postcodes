97dff836e8ea2b1fea329de709f0431fca284e64
Anna Makarudze <amakarudze@gmail.com>
2020-04-03 16:26:46 +0200

    Validate postcodes against Code-Point Open data

--- postcodes_api/views.py
+++ postcodes_api/views.py
@@ -1,3 +1,4 @@
+import csv
 import re
 
 from django.shortcuts import HttpResponse
@@ -10,26 +11,34 @@ def validate_postcode(request, code):
     if len(postcode) < 5 or len(postcode) > 8:
         return HttpResponse(f"Invalid postcode - {postcode}.")
 
-    pattern = r"^(([A-Z]{1,2}[0-9][A-Z0-9]?|ASCN|STHL|TDCU|BBND|[BFS]IQQ|PCRN|TKCA) " \
-              r"?[0-9][A-Z]{2}|BFPO ?[0-9]{1,4}|(KY[0-9]|MSR|VG|AI)[ -]?[0-9]{4}|[A-Z]{2} " \
-              r"?[0-9]{2}|GE ?CX|GIR ?0A{2}|SAN ?TA1)$"
-
-    if re.match(pattern, postcode):
-        # Check for whitespace between outer and inner parts and add one if absent
-        if postcode[-4] == " ":
-            formatted_postcode = postcode
-        else:
-            if len(postcode) == 5:
-                formatted_postcode = postcode[:2] + " " + postcode[2:]
-            elif len(postcode) == 6:
-                formatted_postcode = postcode[:3] + " " + postcode[3:]
-            elif len(postcode) == 7:
-                formatted_postcode = postcode[:4] + " " + postcode[4:]
-            else:
-                raise ValueError("Invalid postcode.")
-
-        message = f"{formatted_postcode} is a valid UK postcode."
-
-        return HttpResponse(message)
+    pattern = r"[A-Z]"
+    if re.match(pattern, postcode[1]):
+        filename = postcode[:2].lower() + ".csv"
     else:
+        filename = postcode[0].lower() + ".csv"
+
+    # Check for whitespace between outer and inner parts and add one if absent
+    if postcode[-4] == " ":
+        formatted_postcode = postcode
+    else:
+        if len(postcode) == 5:
+            formatted_postcode = postcode[:2] + " " + postcode[2:]
+        elif len(postcode) == 6:
+            formatted_postcode = postcode[:3] + " " + postcode[3:]
+        elif len(postcode) == 7:
+            formatted_postcode = postcode[:4] + " " + postcode[4:]
+        else:
+            raise ValueError("Invalid postcode.")
+    try:
+        with open(f"Data/CSV/{filename}") as csv_file:
+            csv_reader = csv.reader(csv_file, delimiter=",")
+            for row in csv_reader:
+                if postcode or formatted_postcode in row:
+                    message = f"{formatted_postcode} is a valid UK postcode."
+
+                    return HttpResponse(message)
+
+                else:
+                    return HttpResponse(f"Invalid postcode - {postcode}.")
+    except FileNotFoundError:
         return HttpResponse(f"Invalid postcode - {postcode}.")
